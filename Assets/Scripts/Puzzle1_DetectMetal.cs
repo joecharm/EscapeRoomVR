ec78c6507f01766b658c3bd97b5f7f31ad9685c4
Joe <919946@swansea.ac.uk>
2020-12-22 22:22:04 +0000

    Added a test area to the Joe scene to add collision between objects and change a flame colour depending on the collision. This will be used in puzzle one.

---
+++ Assets/Scripts/Puzzle1_DetectMetal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle1_DetectMetal : MonoBehaviour
+{
+    // Gameobject of the particles, used later to access the particle component
+    public GameObject particles;
+    // Set the default flame colour, assigned on script start
+    private Color flameColour = Color.yellow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Access the particle system on the supplied game object
+        ParticleSystem.MainModule main = particles.GetComponent<ParticleSystem>().main;
+        // Set the start colour of the flame
+        main.startColor = flameColour;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on collision
+        if (collision.gameObject.name == "Zinc")
+        {
+            // Access the particle system on the supplied game object
+            ParticleSystem.MainModule main = particles.GetComponent<ParticleSystem>().main;
+            // Set the particle colour to green
+            main.startColor = Color.green;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+            // Access the particle system on the supplied game object
+            ParticleSystem.MainModule main = particles.GetComponent<ParticleSystem>().main;
+            // Set the particles back to the default flame colour set as a variable
+            main.startColor = flameColour;
+    }
+}
