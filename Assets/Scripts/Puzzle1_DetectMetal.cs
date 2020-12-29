fb36fbdebbc9eea1d9cb5770e2855405ea9fc663
Joe <919946@swansea.ac.uk>
2020-12-29 18:52:26 +0000

    Completed the flame change for puzzle 1. Each metal will change to the specified colour on the game object (by name)

---
+++ Assets/Scripts/Puzzle1_DetectMetal.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle1_DetectMetal : MonoBehaviour
+{
+    // Gameobject of the particles, used later to access the particle component
+    public GameObject particles;
+
+    // Set the default flame colour, assigned on script start (RGB, default of A is 1)
+    public Color defaultFlameColour = new Color(0.5188679f, 0.3203758f, 0.1270914f);
+
+    // default colour for Teal
+    public Color teal, purple, white, blue, green;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Access the particle system on the supplied game object
+        ParticleSystem.MainModule main = particles.GetComponent<ParticleSystem>().main;
+
+        // Set the start colour of the flame
+        main.startColor = defaultFlameColour;
+
+        // set the shader tint colour to the flame colour to match
+        particles.GetComponent<Renderer>().material.SetColor("_TintColor", defaultFlameColour);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Check for a match with the specified name on collision
+        if (other.gameObject.name == "Teal")
+        {
+            // sets the material shader tint to the colour defined for the metal
+            particles.GetComponent<Renderer>().material.SetColor("_TintColor", teal);
+        }
+
+        //Check for a match with the specified name on collision
+        if (other.gameObject.name == "Purple")
+        {
+            // sets the material shader tint to the colour defined for the metal
+            particles.GetComponent<Renderer>().material.SetColor("_TintColor", purple);
+        }
+
+        //Check for a match with the specified name on collision
+        if (other.gameObject.name == "Blue")
+        {
+            // sets the material shader tint to the colour defined for the metal
+            particles.GetComponent<Renderer>().material.SetColor("_TintColor", blue);
+        }
+
+        //Check for a match with the specified name on collision
+        if (other.gameObject.name == "White")
+        {
+            // sets the material shader tint to the colour defined for the metal
+            particles.GetComponent<Renderer>().material.SetColor("_TintColor", white);
+        }
+
+        //Check for a match with the specified name on collision
+        if (other.gameObject.name == "Green")
+        {
+            // sets the material shader tint to the colour defined for the metal
+            particles.GetComponent<Renderer>().material.SetColor("_TintColor", green);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ParticleSystem.MainModule main = particles.GetComponent<ParticleSystem>().main;
+        main.startColor = defaultFlameColour;
+        particles.GetComponent<Renderer>().material.SetColor("_TintColor", defaultFlameColour);
+
+    }
+}
