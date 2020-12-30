20b0f087f72f1c8a9694261208286e2201074d19
Joe <919946@swansea.ac.uk>
2020-12-30 00:08:27 +0000

    Added working KeyPad to the scene, this is accompained by the animations which are played when a user hits a button. Success and failure materials also added for the buttons. The bunsen burner puzzle also is fully functional but requires cleanup of the colliders & needs an attach point on the metal holders.

---
+++ Assets/Scripts/Game_KeyPad.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_KeyPad : MonoBehaviour
+{
+    private int requiredKey = 3;
+    private bool canHit = true;
+
+    public string checkKey(int passedKey)
+    {
+        if(passedKey == requiredKey)
+        {
+            changeKey();
+            return "success";
+        }
+        else
+        {
+            return "error";
+        }
+    }
+
+    private void changeKey()
+    {
+        switch(requiredKey)
+        {
+            case 3:
+                requiredKey = 2;
+                break;
+            case 2:
+                requiredKey = 8;
+                break;
+            case 8:
+                requiredKey = 7;
+                break;
+            case 7:
+                // success door open
+                break;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/KeyPad_Child.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPad_Child : MonoBehaviour
+{
+    public int keyPadRef;
+    public Game_KeyPad masterKeyPad;
+    public Material errorMaterial, successMaterial;
+
+    private Material defaultMaterial;
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+            //get default
+            defaultMaterial = GetComponent<MeshRenderer>().material;
+
+            Animator anim = GetComponent<Animator>();
+            anim.PlayInFixedTime("Key");
+
+            string returnedState = masterKeyPad.checkKey(keyPadRef);
+
+            StartCoroutine(materialChange(returnedState));
+
+    }
+
+
+    IEnumerator materialChange(string state)
+    {
+        
+        if(state == "success")
+        {
+            GetComponent<MeshRenderer>().material = successMaterial;
+            yield return null;
+        }
+        else if(state == "error" && GetComponent<MeshRenderer>().material.name != "Key_Success")
+        {
+            GetComponent<MeshRenderer>().material = errorMaterial;
+            yield return new WaitForSeconds(1.0f);
+            GetComponent<MeshRenderer>().material = defaultMaterial;
+        }
+        else
+        {
+            Debug.LogError("Incorrect Button State");
+            yield return null;
+        }
+
+    }
+
+
+
+
+
+}
