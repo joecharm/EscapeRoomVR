f9ab7aa7cdb039c0922f0a17002edc9c56a3a836
Joe <919946@swansea.ac.uk>
2021-01-01 21:36:15 +0000

    Added iPad blur to the scene, this is controller by the buttons in front of the microscope. Change the blur intensity will reveal the image and the player can find a number in the image. The iPad is XR grabable.

---
+++ Assets/Scripts/Puzzle3_Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle3_Controller : MonoBehaviour
+{
+    // create a range property of 0-5 which is the blur intensity
+    [Range(0,6)]
+    [SerializeField] public int currentBlurLevel = 4;
+    // access the sprite renderer from the game object in Unity containing the sprite
+    public SpriteRenderer spriteRender;
+    // declare publicly so we can assign sprites at different blur levels to the script
+    public Sprite blur6, blur5, blur4, blur3, blur2, blur1, blur0;
+
+    private void Start()
+    {
+        changeBlur();
+    }
+
+
+    public void changeBlur()
+    {
+        switch (currentBlurLevel)
+        {
+            case 6:
+                spriteRender.sprite = blur6;
+                break;
+            case 5:
+                spriteRender.sprite = blur5;
+                break;
+            case 4:
+                spriteRender.sprite = blur4;
+                break;
+            case 3:
+                spriteRender.sprite = blur3;
+                break;
+            case 2:
+                spriteRender.sprite = blur2;
+                break;
+            case 1:
+                spriteRender.sprite = blur1;
+                break;
+            case 0:
+                spriteRender.sprite = blur0;
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Puzzle3_iPadBlurDecrease.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle3_iPadBlurDecrease : MonoBehaviour
+{
+    // Import the controller script for the puzzle
+    public Puzzle3_Controller controller;
+
+    // on trigger of button collider
+    public void OnTriggerEnter(Collider other)
+    {
+        // VR hand has a collider tagged 'Hand', this will run if the hand hits the button
+        if (other.CompareTag("Hand"))
+        {
+            if(controller.currentBlurLevel >= 0)
+            {
+                // decrement the currentBlurLevel property by 1 on the controller script
+                controller.currentBlurLevel--;
+                // run the changeBlur method which will update the blur to current intensity
+                controller.changeBlur();
+            }
+
+        }
+    }
+
+}
