bafc1bd917b1a5087fbb42ceb968d6d43fad86ad
Joe <919946@swansea.ac.uk>
2021-01-05 21:16:14 +0000

    Added electric box to replace the missing machine. Made the flask interactable in VR. Added another iPad to display puzzle 2 along with an interactable cube for the player to use to help solve the ouzzle

---
+++ Assets/Scripts/CloseLift_EndGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CloseLift_EndGame : MonoBehaviour
+{
+    public GameObject MainDoor, finishText;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Hand"))
+        {
+            // close the door behind the player - get the animator on the door
+            Animator doorAnimation = MainDoor.GetComponent<Animator>();
+            // play the close animation
+            doorAnimation.PlayInFixedTime("LiftDoorClose");
+
+            // Display end of game text
+            finishText.SetActive(true);
+        }
+    }
+}
